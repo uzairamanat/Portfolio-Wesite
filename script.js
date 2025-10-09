34e3fc34987578adaf54a857eb3272fca51e4adc
uzairamanat <uzair.amanat@hotmail.com>
2025-10-09 18:24:21 +0100

    added dark /light mode + animations

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.skills-container, projects-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
+
