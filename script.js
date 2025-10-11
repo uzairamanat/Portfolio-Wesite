41db25ebb3989cbecad64c83ba1efc9885dad70c
uzairamanat <uzair.amanat@hotmail.com>
2025-10-11 18:41:02 +0100

    added links and CV, fixed animation bugs

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// sticky navbar //
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 };
 
+
+// dark and light mode //
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
 darkModeIcon.onclick = () => {
@@ -11,6 +31,8 @@ darkModeIcon.onclick = () => {
     document.body.classList.toggle('dark-mode');
 }
 
+
+// text and scroll animations //
 ScrollReveal({
     reset: true,
     distance: '80px',
